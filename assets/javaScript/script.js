69bb4b0870a157183b212c610a4fb3729d4b08ef
dwc3k5 <dwc1608@gmail.com>
2017-10-01 16:12:06 -0500

    javascript functioning

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,24 +1,28 @@
+/*
+variables not working... need to figure them out for more concise code
+
 var getIndex = document.getElementById('indexFormat');
 var getPortfolio = document.getElementById('portfolioFormat');
 var getContact = document.getElementById('contactFormat');
+*/
 function index(){
-  getIndex.style.display = 'block';
-  getPortfolio.style.display = 'none';
-  getContact.style.display = 'none';
+  document.getElementById('indexFormat').style.display = 'block';
+  document.getElementById('portfolioFormat').style.display = 'none';
+  document.getElementById('contactFormat').style.display = 'none';
 }
 function portfolio(){
-  getIndex.style.display = 'none';
-  getPortfolio.style.display = 'block';
-  getContact.style.display = 'none';
+  document.getElementById('indexFormat').style.display = 'none';
+  document.getElementById('portfolioFormat').style.display = 'block';
+  document.getElementById('contactFormat').style.display = 'none';
 }
 function contact(){
-  getIndex.style.display = 'none';
-  getPortfolio.style.display = 'none';
-  getContact.style.display = 'block';
+  document.getElementById('indexFormat').style.display = 'none';
+  document.getElementById('portfolioFormat').style.display = 'none';
+  document.getElementById('contactFormat').style.display = 'block';
 }
 /*
-test that js is connected and working
-goes off the "David Cox" top left corner
+test function confirming js is connected and working
+triggered by "David Cox" top left corner
 */
 function demo() {
     document.getElementById("test").src= "assets/images/pikachu.jpg";
